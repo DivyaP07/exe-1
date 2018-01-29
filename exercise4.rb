e62174dc425fa8e869f09441ae6cb87f68416046
DivyaP07 <>
2018-01-29 15:26:18 +0530

    add

---
+++ exercise4.rb
@@ -0,0 +1,14 @@
+name = 'divya'
+age = 21
+height = 54
+weight = 150
+eyes = 'brown'
+teeth = 'white'
+
+
+puts "let's talk about #{name}."
+puts "he's #{height} inches tall."
+puts "he's #{weight} pounds heavy."
+puts "he's got #{eyes} eyes and #{teeth} teeth."
+
+puts "if i add #{age}, #{height}, and #{weight} i get #{age + height + weight}."
